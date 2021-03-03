ca24778a7bf7ceb070683d4fb3cfa886f406614d
snsunil215 <42077489+snsunil215@users.noreply.github.com>
2021-03-03 15:44:29 -0800

    Create Dependent.java file
    Hey Rajendra
    
    I Created a Dependent.java file
    i am trying to collaborate to your repo
    
    Regards
    Naga

---
+++ workspace/JavaBasicExamples/src/com/java/practise/pojos/Dependent.java
@@ -0,0 +1,23 @@
+package com.java.practise.pojos;
+
+import java.io.Serializable;
+
+/**
+ * The Class Dependent.
+ */
+public class Dependent implements Serializable, Cloneable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/** The dependent id. */
+	Integer dependentId;
+
+	/** The dependent name. */
+	String dependentName;
+  
+}
+
+// TODO : Generate the Getters and Setter
